6cbe2e8cfd152e4c26026507acdbd2225f4fe5cf
Jianmeng Yu <zhsjyjm@163.com>
2016-12-02 04:55:08 +0000

    Scrabble Interface Implemented

--- app/src/main/java/com/yujianmeng/selp/grabble/FragmentScrabble.java
+++ app/src/main/java/com/yujianmeng/selp/grabble/FragmentScrabble.java
@@ -1,23 +1,266 @@
 package com.yujianmeng.selp.grabble;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by YuJianmeng on 2016/10/6.
  */
 
-public class FragmentScrabble extends Fragment {
+public class FragmentScrabble extends Fragment implements View.OnClickListener{
+
+    private TextView mLevel;
+    private TextView mExp;
+    private TextView mScore;
+    private TextView mDiscard;
+    private ImageView mComplete;
+
+    private TextView mWarning1;
+    private TextView mWarning2;
+
+    private ImageView mInput1;
+    private ImageView mInput2;
+    private ImageView mInput3;
+    private ImageView mInput4;
+    private ImageView mInput5;
+    private ImageView mInput6;
+    private ImageView mInput7;
+
+    private ImageView mButtonA;
+    private ImageView mButtonB;
+    private ImageView mButtonC;
+    private ImageView mButtonD;
+    private ImageView mButtonE;
+    private ImageView mButtonF;
+    private ImageView mButtonG;
+    private ImageView mButtonH;
+    private ImageView mButtonI;
+    private ImageView mButtonJ;
+    private ImageView mButtonK;
+    private ImageView mButtonL;
+    private ImageView mButtonM;
+    private ImageView mButtonN;
+    private ImageView mButtonO;
+    private ImageView mButtonP;
+    private ImageView mButtonQ;
+    private ImageView mButtonR;
+    private ImageView mButtonS;
+    private ImageView mButtonT;
+    private ImageView mButtonU;
+    private ImageView mButtonV;
+    private ImageView mButtonW;
+    private ImageView mButtonX;
+    private ImageView mButtonY;
+    private ImageView mButtonZ;
+
+    private Grabble grabble = new Grabble();
+
+    private int exp = 3400;
+    private int level = 20;
+    private int score = grabble.getPoint();
+
+    private Typeface mFont;
+
+    //Temp stuff
+    private RelativeLayout mInput;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scrabble, container, false);
+
+        mFont = Typeface.createFromAsset(getActivity().getAssets(), "generica_bold.otf");
+
+        mLevel = (TextView) view.findViewById(R.id.scrabble_level_button);
+        mLevel.setTypeface(mFont);
+        mExp = (TextView) view.findViewById(R.id.scrabble_exp_button);
+        mExp.setTypeface(mFont);
+        mScore = (TextView) view.findViewById(R.id.scrabble_score_button);
+        mScore.setTypeface(mFont);
+        mDiscard = (TextView) view.findViewById(R.id.scrabble_discard_button);
+        mDiscard.setTypeface(mFont);
+        mWarning1 = (TextView) view.findViewById(R.id.scrabble_warning1);
+        mWarning1.setTypeface(mFont);
+        mWarning2 = (TextView) view.findViewById(R.id.scrabble_warning2);
+        mWarning2.setTypeface(mFont);
+        mComplete = (ImageView) view.findViewById(R.id.scrabble_complete_button);
+
+        mInput1 = (ImageView) view.findViewById(R.id.scrabble_input_1);
+        mInput1.setImageResource(R.drawable.scrabble_button);
+        mInput2 = (ImageView) view.findViewById(R.id.scrabble_input_2);
+        mInput2.setImageResource(R.drawable.scrabble_button);
+        mInput3 = (ImageView) view.findViewById(R.id.scrabble_input_3);
+        mInput3.setImageResource(R.drawable.scrabble_button);
+        mInput4 = (ImageView) view.findViewById(R.id.scrabble_input_4);
+        mInput4.setImageResource(R.drawable.scrabble_button);
+        mInput5 = (ImageView) view.findViewById(R.id.scrabble_input_5);
+        mInput5.setImageResource(R.drawable.scrabble_button);
+        mInput6 = (ImageView) view.findViewById(R.id.scrabble_input_6);
+        mInput6.setImageResource(R.drawable.scrabble_button);
+        mInput7 = (ImageView) view.findViewById(R.id.scrabble_input_7);
+        mInput7.setImageResource(R.drawable.scrabble_button);
+
+        mButtonA = (ImageView) view.findViewById(R.id.scrabble_input_a);
+        mButtonB = (ImageView) view.findViewById(R.id.scrabble_input_b);
+        mButtonC = (ImageView) view.findViewById(R.id.scrabble_input_c);
+        mButtonD = (ImageView) view.findViewById(R.id.scrabble_input_d);
+        mButtonE = (ImageView) view.findViewById(R.id.scrabble_input_e);
+        mButtonF = (ImageView) view.findViewById(R.id.scrabble_input_f);
+        mButtonG = (ImageView) view.findViewById(R.id.scrabble_input_g);
+        mButtonH = (ImageView) view.findViewById(R.id.scrabble_input_h);
+        mButtonI = (ImageView) view.findViewById(R.id.scrabble_input_i);
+        mButtonJ = (ImageView) view.findViewById(R.id.scrabble_input_j);
+        mButtonK = (ImageView) view.findViewById(R.id.scrabble_input_k);
+        mButtonL = (ImageView) view.findViewById(R.id.scrabble_input_l);
+        mButtonM = (ImageView) view.findViewById(R.id.scrabble_input_m);
+        mButtonN = (ImageView) view.findViewById(R.id.scrabble_input_n);
+        mButtonO = (ImageView) view.findViewById(R.id.scrabble_input_o);
+        mButtonP = (ImageView) view.findViewById(R.id.scrabble_input_p);
+        mButtonQ = (ImageView) view.findViewById(R.id.scrabble_input_q);
+        mButtonR = (ImageView) view.findViewById(R.id.scrabble_input_r);
+        mButtonS = (ImageView) view.findViewById(R.id.scrabble_input_s);
+        mButtonT = (ImageView) view.findViewById(R.id.scrabble_input_t);
+        mButtonU = (ImageView) view.findViewById(R.id.scrabble_input_u);
+        mButtonV = (ImageView) view.findViewById(R.id.scrabble_input_v);
+        mButtonW = (ImageView) view.findViewById(R.id.scrabble_input_w);
+        mButtonX = (ImageView) view.findViewById(R.id.scrabble_input_x);
+        mButtonY = (ImageView) view.findViewById(R.id.scrabble_input_y);
+        mButtonZ = (ImageView) view.findViewById(R.id.scrabble_input_z);
+        mButtonA.setOnClickListener(this);
+        mButtonB.setOnClickListener(this);
+        mButtonC.setOnClickListener(this);
+        mButtonD.setOnClickListener(this);
+        mButtonE.setOnClickListener(this);
+        mButtonF.setOnClickListener(this);
+        mButtonG.setOnClickListener(this);
+        mButtonH.setOnClickListener(this);
+        mButtonI.setOnClickListener(this);
+        mButtonJ.setOnClickListener(this);
+        mButtonK.setOnClickListener(this);
+        mButtonL.setOnClickListener(this);
+        mButtonM.setOnClickListener(this);
+        mButtonN.setOnClickListener(this);
+        mButtonO.setOnClickListener(this);
+        mButtonP.setOnClickListener(this);
+        mButtonQ.setOnClickListener(this);
+        mButtonR.setOnClickListener(this);
+        mButtonS.setOnClickListener(this);
+        mButtonT.setOnClickListener(this);
+        mButtonU.setOnClickListener(this);
+        mButtonV.setOnClickListener(this);
+        mButtonW.setOnClickListener(this);
+        mButtonX.setOnClickListener(this);
+        mButtonY.setOnClickListener(this);
+        mButtonZ.setOnClickListener(this);
+
+        mComplete.setOnClickListener(this);
+        mDiscard.setOnClickListener(this);
+        mDiscard.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                grabble.discardAll();
+                updateUI();
+                return false;
+            }
+        });
+
+        //Temp stuff
+        mInput = (RelativeLayout) view.findViewById(R.id.scrabble_layout_input);
+        mInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addScore(score);
+                updateUI();
+            }
+        });
+
+        updateUI();
         return view;
     }
 
+    public void addScore(int score){
+        int newScore = exp + score;
+        //TODO add level details
+        if (newScore/170>level){
+            level = newScore/170;
+            Toast.makeText(getActivity().getApplicationContext(),
+                    "Leveru Apuda!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        exp = newScore;
+        updateUI();
+    }
+
+    public void updateUI(){
+        score = grabble.getScore();
+        char[] input = grabble.getInput();
+        int point = grabble.getPoint();
+        if (point > 0 ){mInput1.setImageResource(grabble.getImage(input[0]));}
+            else {mInput1.setImageResource(R.drawable.scrabble_button);}
+        if (point > 1 ){mInput2.setImageResource(grabble.getImage(input[1]));}
+            else {mInput2.setImageResource(R.drawable.scrabble_button);}
+        if (point > 2 ){mInput3.setImageResource(grabble.getImage(input[2]));}
+            else {mInput3.setImageResource(R.drawable.scrabble_button);}
+        if (point > 3 ){mInput4.setImageResource(grabble.getImage(input[3]));}
+            else {mInput4.setImageResource(R.drawable.scrabble_button);}
+        if (point > 4 ){mInput5.setImageResource(grabble.getImage(input[4]));}
+            else {mInput5.setImageResource(R.drawable.scrabble_button);}
+        if (point > 5 ){mInput6.setImageResource(grabble.getImage(input[5]));}
+            else {mInput6.setImageResource(R.drawable.scrabble_button);}
+        if (point > 6 ){mInput7.setImageResource(grabble.getImage(input[6]));}
+            else {mInput7.setImageResource(R.drawable.scrabble_button);}
+        mLevel.setText("Level: "+level);
+        mExp.setText("Exp: "+exp);
+        mScore.setText("Total Score: "+score);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.scrabble_input_a: grabble.enterLetter('a'); updateUI(); break;
+            case R.id.scrabble_input_b: grabble.enterLetter('b'); updateUI(); break;
+            case R.id.scrabble_input_c: grabble.enterLetter('c'); updateUI(); break;
+            case R.id.scrabble_input_d: grabble.enterLetter('d'); updateUI(); break;
+            case R.id.scrabble_input_e: grabble.enterLetter('e'); updateUI(); break;
+            case R.id.scrabble_input_f: grabble.enterLetter('f'); updateUI(); break;
+            case R.id.scrabble_input_g: grabble.enterLetter('g'); updateUI(); break;
+            case R.id.scrabble_input_h: grabble.enterLetter('h'); updateUI(); break;
+            case R.id.scrabble_input_i: grabble.enterLetter('i'); updateUI(); break;
+            case R.id.scrabble_input_j: grabble.enterLetter('j'); updateUI(); break;
+            case R.id.scrabble_input_k: grabble.enterLetter('k'); updateUI(); break;
+            case R.id.scrabble_input_l: grabble.enterLetter('l'); updateUI(); break;
+            case R.id.scrabble_input_m: grabble.enterLetter('m'); updateUI(); break;
+            case R.id.scrabble_input_n: grabble.enterLetter('n'); updateUI(); break;
+            case R.id.scrabble_input_o: grabble.enterLetter('o'); updateUI(); break;
+            case R.id.scrabble_input_p: grabble.enterLetter('p'); updateUI(); break;
+            case R.id.scrabble_input_q: grabble.enterLetter('q'); updateUI(); break;
+            case R.id.scrabble_input_r: grabble.enterLetter('r'); updateUI(); break;
+            case R.id.scrabble_input_s: grabble.enterLetter('s'); updateUI(); break;
+            case R.id.scrabble_input_t: grabble.enterLetter('t'); updateUI(); break;
+            case R.id.scrabble_input_u: grabble.enterLetter('u'); updateUI(); break;
+            case R.id.scrabble_input_v: grabble.enterLetter('v'); updateUI(); break;
+            case R.id.scrabble_input_w: grabble.enterLetter('w'); updateUI(); break;
+            case R.id.scrabble_input_x: grabble.enterLetter('x'); updateUI(); break;
+            case R.id.scrabble_input_y: grabble.enterLetter('y'); updateUI(); break;
+            case R.id.scrabble_input_z: grabble.enterLetter('z'); updateUI(); break;
+
+            case R.id.scrabble_complete_button:
+                if (grabble.getPoint() == 7){
+                    addScore(grabble.completeWord());
+                };break;
+            case R.id.scrabble_discard_button:
+                grabble.discardLetter();
+                updateUI();
+                break;
+        }
+    }
 }
 
---
+++ app/src/main/java/com/yujianmeng/selp/grabble/Grabble.java
@@ -0,0 +1,155 @@
+package com.yujianmeng.selp.grabble;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by YuJianmeng on 2016/12/1.
+ */
+public class Grabble {
+
+    private char[] input = new char[7];
+    private int inputpoint = 0;
+    private int[] letters = new int[26];
+    private String alphabet = "abcdefghijklmnopqrstuvwxyz";
+    private HashMap<Character,Integer> scoreSG = new HashMap<Character, Integer>() {{
+        put('a',3 );put('b',20);put('c',13);put('d',10);put('e',1 );
+        put('f',15);put('g',18);put('h',9 );put('i',5 );put('j',25);
+        put('k',22);put('l',11);put('m',14);put('n',6 );put('o',4 );
+        put('p',19);put('q',24);put('r',8 );put('s',7 );put('t',2 );
+        put('u',12);put('v',21);put('w',17);put('x',23);put('y',16);
+        put('z',26);
+    }};
+
+    //TODO read from asset
+    private ArrayList<String> dictionary = new ArrayList<>(Arrays.asList("harambe","drzaius"));
+
+    public Grabble(){
+        //TODO Initialize letter storage;
+        Arrays.fill(letters,2);
+    }
+
+    public char[] getInput(){
+        return input;
+    }
+
+    public boolean isWord(){
+        String outcome = String.valueOf(input);
+        if (inputpoint == 7){
+            return dictionary.contains(outcome);
+        }
+        return false;
+    }
+
+    public int getScore(){
+        int score = 0;
+        int i = inputpoint;
+        while (i > 0){
+            i--;
+            score = score + scoreSG.get(input[i]);
+        }
+        return score;
+    }
+
+    public int completeWord(){
+        if(isWord()){
+            int score = getScore();
+            inputpoint = 0;
+            return score;
+        }
+        return 0;
+    }
+
+    public void enterLetter(char a){
+        int i = charToInt(a);
+        if (inputpoint < 7 && letters[i] > 0) {
+            input[inputpoint] = alphabet.charAt(i);
+            inputpoint++;
+            letters[i]--;
+        }
+    }
+
+    public void discardLetter(){
+        if(inputpoint > 0){
+            inputpoint--;
+            letters[charToInt(input[inputpoint])]++;
+        }
+    }
+
+    public void discardAll(){
+        while (inputpoint > 0){
+            discardLetter();
+        }
+    }
+
+    public int charToInt(char c){
+        char[] out = alphabet.toCharArray();
+        for (int i = 0; i < 26; i++){
+            if (c == out[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getPoint(){
+        return inputpoint;
+    }
+
+    //Used to get image resource int of the character
+    public int getImage(char c){
+        if (c == 'a'){return R.drawable.scrabble_button_a;}
+        if (c == 'b'){return R.drawable.scrabble_button_b;}
+        if (c == 'c'){return R.drawable.scrabble_button_c;}
+        if (c == 'd'){return R.drawable.scrabble_button_d;}
+        if (c == 'e'){return R.drawable.scrabble_button_e;}
+        if (c == 'f'){return R.drawable.scrabble_button_f;}
+        if (c == 'g'){return R.drawable.scrabble_button_g;}
+        if (c == 'h'){return R.drawable.scrabble_button_h;}
+        if (c == 'i'){return R.drawable.scrabble_button_i;}
+        if (c == 'j'){return R.drawable.scrabble_button_j;}
+        if (c == 'k'){return R.drawable.scrabble_button_k;}
+        if (c == 'l'){return R.drawable.scrabble_button_l;}
+        if (c == 'm'){return R.drawable.scrabble_button_m;}
+        if (c == 'n'){return R.drawable.scrabble_button_n;}
+        if (c == 'o'){return R.drawable.scrabble_button_o;}
+        if (c == 'p'){return R.drawable.scrabble_button_p;}
+        if (c == 'q'){return R.drawable.scrabble_button_q;}
+        if (c == 'r'){return R.drawable.scrabble_button_r;}
+        if (c == 's'){return R.drawable.scrabble_button_s;}
+        if (c == 't'){return R.drawable.scrabble_button_t;}
+        if (c == 'u'){return R.drawable.scrabble_button_u;}
+        if (c == 'v'){return R.drawable.scrabble_button_v;}
+        if (c == 'w'){return R.drawable.scrabble_button_w;}
+        if (c == 'x'){return R.drawable.scrabble_button_x;}
+        if (c == 'y'){return R.drawable.scrabble_button_y;}
+        if (c == 'z'){return R.drawable.scrabble_button_z;}
+        return R.drawable.scrabble_button;
+    }
+
+    /* TODO use this to test further, or remove it
+    public static void main(String[] args){
+        Grabble grabble = new Grabble();
+        grabble.enterLetter('a');
+        grabble.enterLetter('a');
+        grabble.enterLetter('a');
+        char[] input = grabble.getInput();
+        System.out.print(String.valueOf(input));
+        System.out.print('\n');
+        grabble.discardAll();
+        grabble.enterLetter('h');
+        grabble.enterLetter('a');
+        grabble.enterLetter('r');
+        grabble.enterLetter('a');
+        grabble.enterLetter('m');
+        grabble.enterLetter('b');
+        grabble.enterLetter('e');
+        input = grabble.getInput();
+        for (int i = 0; i < 7; i++){
+            System.out.print(input[i]);
+        }
+        System.out.print('\n');
+        System.out.print(grabble.completeWord());
+    }*/
+}
